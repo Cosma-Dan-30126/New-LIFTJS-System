8f6931f24de77560b78008912134e12b75f71385
Cosma-Dan-30126 <126558035+Cosma-Dan-30126@users.noreply.github.com>
2026-02-24 11:05:33 +0200

    Added Lift states

--- Code/elevator.js
+++ Code/elevator.js
@@ -2,6 +2,13 @@ let posA=1;
 let posB=7;
 const totalFloors=7;
 
+// Urmatoarea implementare pe care as face-o este sa creez un sistem de stari pentru a spune in ce stare se afla liftul. Ne ajuta la deschisul usilor
+const States=[
+    {name: 'IDLE', description: 'Wait/Busy'},
+    {name: 'DOORS_OPEN', leftTransform: 'translateX(-100%)',rightTransform: 'translateX(100%)'},
+    {name: 'DOORS_CLOSED', leftTransform: 'translateX(0)', rightTransform: 'translateX(0%)' }
+];
+
 function moveLift(reqFloor){
     let distA=Math.abs(reqFloor-posA);
     let distB=Math.abs(reqFloor-posB);
@@ -32,6 +39,6 @@ function goToFloor(floor,liftId){
     liftId==="A" ? posA= floor : posB=floor;
     console.log(`Liftul cu Id ${liftId} se misca catre etajul selectat ${floor}`);
 
-// Urmatoarea implementare pe care as face-o este sa creez un sistem de stari pentru a spune in ce stare se afla liftul. Ne ajuta la deschisul usilor
+
 
 }
