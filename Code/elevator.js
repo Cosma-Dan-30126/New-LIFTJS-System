b5e9e2c45783febe761a94af5355dfe772bb5f10
Cosma-Dan-30126 <126558035+Cosma-Dan-30126@users.noreply.github.com>
2026-02-24 11:51:31 +0200

    Updated moving logic, added a list to check if the lifts are free or not . Next is goToFloor Logic update

--- Code/elevator.js
+++ Code/elevator.js
@@ -1,36 +1,52 @@
-let posA=1;
-let posB=7;
 const totalFloors=7;
 
+//Mai degraba creez un obiect nou pentru lifturi in care le da si pozitia, si starea
+const lifts= {
+    A:{pos: 1, state: States.IDLE, element: document.getElementById('lift_A')},
+    B:{pos: 7, state: States.IDLE, element: document.getElementById('lift_B')}
+
+}
+
 // Urmatoarea implementare pe care as face-o este sa creez un sistem de stari pentru a spune in ce stare se afla liftul. Ne ajuta la deschisul usilor
-const States=[
-    {name: 'IDLE', description: 'Wait/Busy'},
-    {name: 'DOORS_OPEN', leftTransform: 'translateX(-100%)',rightTransform: 'translateX(100%)'},
-    {name: 'DOORS_CLOSED', leftTransform: 'translateX(0)', rightTransform: 'translateX(0%)' }
-];
+const States= {
+    IDLE: 'IDLE',
+    MOVING: 'MOVING', //Miscare. O sa o folosim asta si pentru a putea sa spunem cand un lift este busy pentru a-l selecta pe celalalt, 
+    DOORS_OPEN: 'OPENING',  //Tranzitie
+    DOORS_CLOSING: 'CLOSING'  //Tranzitie
+};
+
 
 function moveLift(reqFloor){
-    let distA=Math.abs(reqFloor-posA);
-    let distB=Math.abs(reqFloor-posB);
+    let availableLifts = [];
 
-    //distA <= distB ? goToFloor(reqFloor, "A")
-                 //  : goToFloor(reqFloor, "B")
+    for( let id in lifts){
+        if(lifts[id].state=== States.IDLE){
+            availableLifts.push(id); 
+        }
+    }
 
-    if(distA === distB) 
-        {
-            goToFloor(reqFloor, "A")
-} else if(distA < distB)
-{
-    goToFloor(reqFloor, "A")
-} else {
-    goToFloor(reqFloor, "B")
+    if (availableLifts.length===0){
+        console.log("Lifturile sunt ocupate, asteapta eliberarea unuia dintre ele!")
+        return;
+    }
+
+    let chosenId;
+    if(availableLifts.length==2){
+        let distA = Math.abs(reqFloor - lifts.A.pos);
+        let distB = Math.abs(reqFloor - lifts.B.pos);
+        chosenId = (distA <= distB) ? "A" : "B";
+    } else {
+        chosenId=availableLifts[0];
+    }
+goToFloor(reqFloor,chosenId);
+
 }
-}
+
 
 function goToFloor(floor,liftId){
     const procent= (floor-1)*(100/totalFloors);
     const liftElement= document.getElementById(`lift_${liftId}`);
-   
+
     if (liftElement) {
         liftElement.style.bottom = `${procent}%`;
                 liftId === "A" ? posA = floor : posB = floor;
